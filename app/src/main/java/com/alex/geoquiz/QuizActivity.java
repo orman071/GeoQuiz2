b25f6e95e8817c0e437ff33bbf21c060ca07ef06
Alexandr <orman071@gmail.com>
2017-09-02 00:07:25 +0400

    123

--- app/src/main/java/com/alex/geoquiz/QuizActivity.java
+++ app/src/main/java/com/alex/geoquiz/QuizActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class QuizActivity extends AppCompatActivity {
+    private int mTrueButton;
+    private int mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
     }
 }

dcca8cd23e729c4f43e45035bd02e4c34bf54cac
Alexandr <orman071@gmail.com>
2017-09-12 23:52:14 +0400

    дополнения 12.09.2017

--- app/src/main/java/com/alex/geoquiz/QuizActivity.java
+++ app/src/main/java/com/alex/geoquiz/QuizActivity.java
@@ -3,15 +3,19 @@ package com.alex.geoquiz;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mPreviousButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[]{
@@ -46,13 +50,24 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1)% mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
 
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +78,18 @@ public class QuizActivity extends AppCompatActivity {
 
             }
         });
+
+        mPreviousButton = (ImageButton) findViewById(R.id.previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + mQuestionBank.length -1) % mQuestionBank.length;
+                updateQuestion();
+
+
+            }
+        });
+
         updateQuestion();
 
         mTrueButton = (Button) findViewById(R.id.true_button);
@@ -85,5 +112,32 @@ public class QuizActivity extends AppCompatActivity {
 
 
 
+
+
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
